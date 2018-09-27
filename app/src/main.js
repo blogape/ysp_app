91d546873784c4fa84afa337b468a3eead793bf4
zengts <905080195@qq.com>
2018-09-27 20:43:47 +0800

    code

--- app/src/main.js
+++ app/src/main.js
@@ -3,9 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import Vant from 'vant';
+// eslint-disable-next
 import services from './utils/request.js';
 import 'vant/lib/vant-css/index.css';
-
 
 Vue.use(Vant);
 Vue.config.productionTip = false;
--- app/src/utils/history.js
+++ app/src/utils/history.js
@@ -1,4 +1,3 @@
 import createHashHistory from 'history/createHashHistory'
-
 const customHistory = createHashHistory()
 export default customHistory;
